b5ad1b4314603b8b5ed9a1c7b7b971899f327407
KimaniSarah <bibokimani@gmailcom>
2023-04-22 00:49:05 -0700

    printf2

--- printf2.c
+++ printf2.c
@@ -2,6 +2,23 @@
 #include"main.h"
 #include <stdarg.h>
 /**
+ * digit_count - count how many digits are in h
+ * @d:the number to be counted
+ * Return:number of digits in h
+ */
+int digit_count(int d)
+{
+	int digits = 0;
+
+	if (d >= 9)
+	{
+		digits += digit_count(d / 10);
+	}
+	putchar((d % 10) + '0');
+	digits++;
+	return (digits);
+}
+/**
  * _printf - produces output according to a format.
  * @format:character string
  * Return:the number of characters printed (excluding the null byte)
@@ -9,8 +26,7 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int count = 0, c;
-	char *s;
+	int count = 0, d;
 
 	va_start(args, format);
 	while (*format)
@@ -18,25 +34,16 @@ int _printf(const char *format, ...)
 		if (*format == '%')
 		{
 			format++;
-			if (*format == 'c')
+			if (*format == 'd' || *format == 'i')
 			{
-				c = va_arg(args, int);
-				putchar(c);
-				count++;
-			}
-			else if (*format == 's')
-			{
-				s = va_arg(args, char *);
-				while (*s)
+				d = va_arg(args, int);
+				if (d < 0)
 				{
-					putchar(*s++);
+					putchar('-');
 					count++;
+					d = -d;
 				}
-			}
-			else if (*format == '%')
-			{
-				putchar('%');
-				count++;
+				count += digit_count(d);
 			}
 			else
 			{
@@ -44,16 +51,14 @@ int _printf(const char *format, ...)
 				putchar(*format);
 				count += 2;
 			}
-			format++;
 		}
 		else
 		{
 			putchar(*format);
 			count++;
-			format++;
 		}
+		format++;
 	}
 	va_end(args);
 	return (count);
 }
-
